14c99b7731fb09806796396529dc185331c1c483
RevolverSnake412 <oussag@hotmail.fr>
2023-09-21 22:01:45 +0100

    changes

--- opcodes_II.c
+++ opcodes_II.c
@@ -29,16 +29,20 @@ void add(stack_t **stack, int line)
 */
 void sub(stack_t **stack, int line)
 {
-	if (*stack == NULL || (**stack).next == NULL)
+	stack_t *current = *stack;
+	int temp;
+
+	if (current == NULL || current->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", line);
 		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
-	(**stack).next->n = (**stack).next->n - (**stack).n;
-	*stack = (**stack).next;
+	temp = current->n - current->next->n;
+	current->next->n = temp;
+	*stack = current->next;
+	free(current);
 }
-
 
 /**
  * div - divs the last two elements
@@ -47,21 +51,23 @@ void sub(stack_t **stack, int line)
 */
 void _div(stack_t **stack, int line)
 {
-	if (*stack == NULL || (**stack).next == NULL)
-	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", line);
-		free_stack(stack);
-		exit(EXIT_FAILURE);
-	}
-	if ((**stack).n == 0)
-	{
-		fprintf(stderr, "L%d: division by zero\n", line);
-		free_stack(stack);
-		exit(EXIT_FAILURE);
-	}
+	stack_t *current = *stack;
+	int temp;
 
-	(**stack).next->n = (**stack).next->n / (**stack).n;
-	*stack = (**stack).next;
+	if (current == NULL || current->next == NULL || current->n == 0)
+	{
+    	if (current == NULL || current->next == NULL)
+        	fprintf(stderr, "L%d: can't div, stack too short\n", line);
+		else
+			fprintf(stderr, "L%d: division by zero\n", line);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+}
+
+	temp = current->n / current->next->n;
+	current->next->n = temp;
+	*stack = current->next;
+	free(current);
 }
 
 /**
@@ -71,14 +77,19 @@ void _div(stack_t **stack, int line)
 */
 void mul(stack_t **stack, int line)
 {
-	if (*stack == NULL || (**stack).next == NULL)
+	stack_t *current = *stack;
+	int temp;
+
+	if (current == NULL || current->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't mul, stack too short\n", line);
 		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
-	(**stack).next->n = (**stack).next->n * (**stack).n;
-	*stack = (**stack).next;
+	temp = current->n * current->next->n;
+	current->next->n = temp;
+	*stack = current->next;
+	free(current);
 }
 
 /**
@@ -88,12 +99,21 @@ void mul(stack_t **stack, int line)
 */
 void mod(stack_t **stack, int line)
 {
-	if (*stack == NULL || (**stack).next == NULL)
+	stack_t *current = *stack;
+	int temp;
+
+	if (current == NULL || current->next == NULL || current->n == 0)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line);
+    	if (current == NULL || current->next == NULL)
+        	fprintf(stderr, "L%d: can't mod, stack too short\n", line);
+		else
+			fprintf(stderr, "L%d: division by zero\n", line);
 		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
-	(**stack).next->n = (**stack).next->n % (**stack).n;
-	*stack = (**stack).next;
+
+	temp = current->n % current->next->n;
+	current->next->n = temp;
+	*stack = current->next;
+	free(current);
 }
