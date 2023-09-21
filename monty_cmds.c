16d6d42424761fb59d223ad659857b555abb50a7
RevolverSnake412 <oussag@hotmail.fr>
2023-09-21 17:02:01 +0100

    moving nop to a new file

--- monty_cmds.c
+++ monty_cmds.c
@@ -65,14 +65,3 @@ void pall(stack_t **stack)
 		}
 	}
 }
-
-/**
- * nop - does nothing just like my cat
- * @stack: our pure stack
- * @counter: line count
-*/
-void nop(stack_t **stack, unsigned int counter)
-{
-	(void) stack;
-	(void) counter;
-}
---
+++ void.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing just like my cat
+ * @stack: our pure stack
+ * @counter: line count
+*/
+void nop(stack_t **stack, unsigned int counter)
+{
+	(void) stack;
+	(void) counter;
+}
